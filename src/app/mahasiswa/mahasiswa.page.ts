3ff770c19391a8cce1f020870221c293435e4937
wikela17 <wikelaela22@gmail.com>
2024-11-11 13:49:32 +0700

    first commit

---
+++ src/app/mahasiswa/mahasiswa.page.ts
@@ -0,0 +1,174 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { ModalController, AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-mahasiswa',
+  templateUrl: './mahasiswa.page.html',
+  styleUrls: ['./mahasiswa.page.scss'],
+})
+export class MahasiswaPage implements OnInit {
+  dataMahasiswa: any;
+  modalTambah: any;
+  modalEdit: any;
+  id: any;
+  nama: any;
+  jurusan: any;
+
+  constructor(
+    private api: ApiService,
+    private modal: ModalController,
+    private alertController: AlertController // Tambahkan AlertController
+  ) { }
+
+  ngOnInit() {
+    this.getMahasiswa();
+  }
+
+  // Mendapatkan data mahasiswa
+  getMahasiswa() {
+    this.api.tampil('tampil.php').subscribe({
+      next: (res: any) => {
+        console.log('sukses', res);
+        this.dataMahasiswa = res;
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
+  }
+
+  // Reset fields in the modal
+  resetModal() {
+    this.id = null;
+    this.nama = '';
+    this.jurusan = '';
+  }
+
+  // Membuka modal tambah data
+  openModalTambah(isOpen: boolean) {
+    this.modalTambah = isOpen;
+    this.resetModal();
+    this.modalTambah = true;
+    this.modalEdit = false;
+  }
+
+  // Membuka modal edit data
+  openModalEdit(isOpen: boolean, idget: any) {
+    this.modalEdit = isOpen;
+    this.id = idget;
+    console.log(this.id);
+    this.ambilMahasiswa(this.id);
+    this.modalTambah = false;
+    this.modalEdit = true;
+  }
+
+  // Tutup modal dan reset fields
+  cancel() {
+    this.modal.dismiss();
+    this.modalTambah = false;
+    this.modalEdit = false;
+    this.resetModal();
+  }
+
+  // Tambah data mahasiswa
+  tambahMahasiswa() {
+    if (this.nama != '' && this.jurusan != '') {
+      let data = {
+        nama: this.nama,
+        jurusan: this.jurusan,
+      }
+      this.api.tambah(data, 'tambah.php')
+        .subscribe({
+          next: (hasil: any) => {
+            this.resetModal();
+            console.log('berhasil tambah mahasiswa');
+            this.getMahasiswa();
+            this.modalTambah = false;
+            this.modal.dismiss();
+          },
+          error: (err: any) => {
+            console.log('gagal tambah mahasiswa');
+          }
+        })
+    } else {
+      console.log('gagal tambah mahasiswa karena masih ada data yg kosong');
+    }
+  }
+
+  // Konfirmasi hapus data
+  async confirmHapus(id: any) {
+    const alert = await this.alertController.create({
+      header: 'Konfirmasi Hapus',
+      message: 'Apakah data ingin dihapus?',
+      buttons: [
+        {
+          text: 'Tidak',
+          role: 'cancel',
+          handler: () => {
+            console.log('Penghapusan dibatalkan');
+          }
+        },
+        {
+          text: 'Ya',
+          handler: () => {
+            this.hapusMahasiswa(id);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  // Hapus data mahasiswa
+  hapusMahasiswa(id: any) {
+    this.api.hapus(id, 'hapus.php?id=').subscribe({
+      next: (res: any) => {
+        console.log('sukses', res);
+        this.getMahasiswa();
+        console.log('berhasil hapus data');
+      },
+      error: (error: any) => {
+        console.log('gagal');
+      }
+    })
+  }
+
+  // Ambil data mahasiswa berdasarkan ID
+  ambilMahasiswa(id: any) {
+    this.api.lihat(id, 'lihat.php?id=').subscribe({
+      next: (hasil: any) => {
+        console.log('sukses', hasil);
+        let mahasiswa = hasil;
+        this.id = mahasiswa.id;
+        this.nama = mahasiswa.nama;
+        this.jurusan = mahasiswa.jurusan;
+      },
+      error: (error: any) => {
+        console.log('gagal ambil data');
+      }
+    })
+  }
+
+  // Edit data mahasiswa
+  editMahasiswa() {
+    let data = {
+      id: this.id,
+      nama: this.nama,
+      jurusan: this.jurusan
+    }
+    this.api.edit(data, 'edit.php').subscribe({
+      next: (hasil: any) => {
+        console.log(hasil);
+        this.resetModal();
+        this.getMahasiswa();
+        console.log('berhasil edit Mahasiswa');
+        this.modalEdit = false;
+        this.modal.dismiss();
+      },
+      error: (err: any) => {
+        console.log('gagal edit Mahasiswa');
+      }
+    })
+  }
+}
